ce5c75c9efd605a0f4a1046e7b493074319fc662
argenoriton <gabriel_cassuriaga@hotmail.com>
2022-09-16 23:58:39 -0300

    2 files added

---
+++ 0.2.1.js
@@ -0,0 +1,35 @@
+const anime = {
+  nome: 'Code Geass: Lelouch of the Rebellion',
+  descricao:
+    'O governo de Britânia torna-se uma potência mundial que tiraniza outros países e os torna seus vassalos políticos, até que o estudante Lelouch Lamperouge descobre uma arma do governo e inicia uma revolução.',
+  numdeEpisodios: 50,
+  personagensPrincipais: [
+    'Lelouch vi Brittania',
+    'C.C.',
+    'Kallen Stadtfeld',
+    'Suzaku Kururugi',
+    'Shirley Fenette',
+  ],
+};
+
+const anime2 = {
+  nome: 'Kaguya-sama: Love is a War',
+  descricao:
+    'Amor é guerra! O presidente Shirogane e a vice-presidente Shinomiya estão engajados em convencer um ao outro à declarar seu amor. Aquele que declarar primeiro perde.',
+  numdeEpisodios: 37,
+  personagensPrincipais: [
+    'Miyuki Shirogane',
+    'Kaguya Shinomiya',
+    'Chika Fujiwara',
+    'Yu Ishigami',
+    'Miko Iino',
+  ],
+};
+
+const anime3 = {
+  nome: 'Sonny Boy',
+  numdeEpisodios: 12,
+  descricao:
+    'Um colégio vai parar em um lugar completamente diferente levando junto os estudantes, agora eles terão que descobrir como voltar para casa explorando seus poderes recém descobertos.',
+  personagensPrincipais: ['Nagara', 'Nozomi', 'Mizuho', 'Rajidani'],
+};
