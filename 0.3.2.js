e9a8395d983815326be9acaf4cea4be3a499f10a
argenoriton <gabriel_cassuriaga@hotmail.com>
2022-10-01 00:36:22 -0300

    1 file modified

--- 0.3.2.js
+++ 0.3.2.js
@@ -14,7 +14,25 @@ const tarefas = [
     concluida: true,
     dias: 60,
   },
+  {
+    titulo: 'Assistir Amazon',
+    concluida: false,
+    dias: 10,
+  },
+  {
+    titulo: 'Tomar Tereré',
+    concluida: false,
+    dias: 5,
+  },
+  {
+    titulo: 'Curtir uma Festa',
+    concluida: true,
+    dias: 60,
+  },
 ];
 
-const concluidas = tarefas.filter((tarefas) => tarefas.concluida === true);
+const concluidas = tarefas.filter(({ concluida }) => concluida === true);
+const naoConcluidas = tarefas.filter(({ concluida }) => concluida === false);
+
+const concluidas = concluidas.filter(({ dias }) => dias < 30);
 console.log(concluidas);
